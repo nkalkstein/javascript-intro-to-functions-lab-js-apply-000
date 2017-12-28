f8c4e0fe5dbd3cc069486f69cb4c17af53a3b997
Noah Kalkstein <nkalkstein@gmail.com>
2017-12-28 06:08:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowerCase) {
+  if (string.toLowerCase() === lowercase) {
     return "I can't hear you"
-  } else if (string.toUpperCase() === upperCase) {
+  } else if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
   } else if (string = "I love you, Grandma.") {
         return "I love you, too."
